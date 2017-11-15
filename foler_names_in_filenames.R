ac1ddd39fb4dc9e046ea93f890fda603c3ff5792
Harsh Anuj <harsh.anuj@gmail.com>
2017-11-15 20:40:22 +0530

    Add files via upload

---
+++ foler_names_in_filenames.R
@@ -0,0 +1,40 @@
+#### R function to prefix names of subfolders within a parent folder to the names of files within the subfolders ####
+#### can be used for specifc types of files such as ".pdf", ".xlsx" etc ####
+
+
+folder_names_in_filenames<-function(parent_folder,filetype) {
+
+  dirs<-dir(parent_folder,full.names=T,include.dirs=F)
+
+  print(dirs)
+
+  files<-list.files(parent_folder,pattern=filetype,recursive=T,full.names=F)
+
+  print(files)
+
+  new_fileNames<-as.vector(length(files))
+
+      for (i in 1:length(files)) {
+  
+                                  locs<-regexpr(pattern="/",files[i],fixed=T)[1]
+
+                                  b<-substr(files[i],1,locs-1)
+  
+                                  a<-gregexpr(pattern="/",files[i],fixed=T)
+  
+                                  a<-unlist(a)
+  
+                                  a<-max(a)
+  
+                                  c<-substr(files[i],a+1,1000)
+  
+                                  d<-paste0(b,"_",c)
+  
+                                  new_fileNames[i]<-gsub(c,d,files[i],fixed=T)
+                                 }
+
+print(new_fileNames)
+
+file.rename(paste0(parent_folder,files),
+             paste0(parent_folder,new_fileNames))
+}
